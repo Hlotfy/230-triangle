a370a5c905969a8b9fa6c450625af234104ed4a0
Chris Thurber <cthurber@me.com>
2015-09-13 23:12:03 -0400

    Added getWidth and Height methods to Triangle class

--- Triangle.java
+++ Triangle.java
@@ -1,11 +1,17 @@
 
 public class Triangle {
+
+	//Returns Triangle's Height
 	public int getHeight() {
 		return height;
 	}
+
+	//Returns Triangle's Width
 	public int getWidth() {
 		return width;
 	}
+
+	//Return's the area of the Triangle
 	public int getArea() {
 		return 0.5(width*height);
 	}
